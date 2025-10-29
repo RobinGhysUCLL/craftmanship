54ba5f68c06afa1a6d03157a2b2ed9249247f61c
RobinGhysUcll <robin.ghys@student.ucll.be>
2025-10-29 22:34:14 +0100

    Added .env local file to docker compose

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-set -e  # Stop bij fouten
+set -e
 
 echo "🚀 Startscript gestart..."
 
 cd /craftmanship/craftmanship
+echo "📁 Huidige map: $(pwd)"
+ls -a | grep env || echo "⚠️ Geen .env gevonden in $(pwd)"
 
 echo "🐳 Building and starting with Docker Compose..."
+docker compose --env-file .env down
 docker compose --env-file .env up -d --build
-
