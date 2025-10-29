96ed92ab858f67d845c0e066dfee4ba26fd7fa27
RobinGhysUcll <robin.ghys@student.ucll.be>
2025-10-29 19:08:57 +0100

    Added user controller with login endpoint

---
+++ src/main/java/be/ucll/group8/craftmanshipgroep8/user/controller/UserController.java
@@ -0,0 +1,26 @@
+package be.ucll.group8.craftmanshipgroep8.user.controller;
+
+import be.ucll.group8.craftmanshipgroep8.user.controller.Dto.AuthenticationRequest;
+import be.ucll.group8.craftmanshipgroep8.user.controller.Dto.AuthenticationResponse;
+import be.ucll.group8.craftmanshipgroep8.user.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public AuthenticationResponse login(@RequestBody AuthenticationRequest authenticationRequest) {
+        return userService.authenticate(authenticationRequest.username(), authenticationRequest.password());
+    }
+
+}
