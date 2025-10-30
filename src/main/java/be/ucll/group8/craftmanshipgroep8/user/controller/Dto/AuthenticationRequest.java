41d8536321885c2aaf8d2098f3aabaa44f2504c4
RobinGhysUcll <robin.ghys@student.ucll.be>
2025-10-30 10:35:28 +0100

    Validation added to AuthenticationRequest.java and changed from username login to email login

--- src/main/java/be/ucll/group8/craftmanshipgroep8/user/controller/Dto/AuthenticationRequest.java
+++ src/main/java/be/ucll/group8/craftmanshipgroep8/user/controller/Dto/AuthenticationRequest.java
@@ -1,7 +1,11 @@
 package be.ucll.group8.craftmanshipgroep8.user.controller.Dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public record AuthenticationRequest(
-    String username,
+    @NotNull(message = "Email cannot be null.")
+    String email,
+    @NotNull(message = "Password cannot be null.")
     String password
 ) {
 }
