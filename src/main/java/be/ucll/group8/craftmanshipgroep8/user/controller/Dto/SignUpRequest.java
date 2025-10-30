18d11f69c0b7a98364170b040a0067b44517a8f6
RobinGhysUcll <robin.ghys@student.ucll.be>
2025-10-30 10:45:53 +0100

    Better name convention

--- src/main/java/be/ucll/group8/craftmanshipgroep8/user/controller/Dto/SignUpRequest.java
+++ src/main/java/be/ucll/group8/craftmanshipgroep8/user/controller/Dto/SignUpRequest.java
@@ -3,7 +3,7 @@ package be.ucll.group8.craftmanshipgroep8.user.controller.Dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record SignUpInput(
+public record SignUpRequest(
     @NotBlank
     String username,
     @NotBlank
