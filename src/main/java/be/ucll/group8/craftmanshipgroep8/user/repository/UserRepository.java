214bbba5c166daedec87d2425b27314c43e24d8f
RobinGhysUcll <robin.ghys@student.ucll.be>
2025-11-01 21:53:35 +0100

    Changed UUID to UserId since user now uses UserId

--- src/main/java/be/ucll/group8/craftmanshipgroep8/user/repository/UserRepository.java
+++ src/main/java/be/ucll/group8/craftmanshipgroep8/user/repository/UserRepository.java
@@ -1,17 +1,17 @@
 package be.ucll.group8.craftmanshipgroep8.user.repository;
 
 import be.ucll.group8.craftmanshipgroep8.user.domain.User;
+import be.ucll.group8.craftmanshipgroep8.user.domain.UserId;
 import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UserId> {
     User findByUserName(String userName);
 
     User findUserByUserName(String userName);
 
-    User findUserByEmail(@Email(message = "Email format is invalid, example email format: user@example.com") String email);
+    User findUserByEmail(
+            @Email(message = "Email format is invalid, example email format: user@example.com") String email);
 }
